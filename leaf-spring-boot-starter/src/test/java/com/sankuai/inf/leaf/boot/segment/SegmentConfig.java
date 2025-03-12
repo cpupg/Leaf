3849b20b39ca259d5fd8e78027c6968f1e951b6d
chen <39870481+cpupg@users.noreply.github.com>
2025-03-12 23:57:22 +0800

    增加数据源bean

--- leaf-spring-boot-starter/src/test/java/com/sankuai/inf/leaf/boot/segment/SegmentConfig.java
+++ leaf-spring-boot-starter/src/test/java/com/sankuai/inf/leaf/boot/segment/SegmentConfig.java
@@ -1,24 +1,21 @@
 package com.sankuai.inf.leaf.boot.segment;
 
-import org.apache.ibatis.datasource.pooled.PooledDataSource;
 import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.ibatis.transaction.TransactionFactory;
 import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-import java.util.Properties;
+import javax.sql.DataSource;
 
 @Configuration
 public class SegmentConfig {
     @Bean
-    public SqlSessionFactory getSqlSessionFactory() throws Exception {
-        Properties properties = new Properties();
-        properties.load(SegmentConfig.class.getResourceAsStream("/leaf.properties"));
-        PooledDataSource dataSource = new PooledDataSource();
-        dataSource.setDriverProperties(properties);
+    public SqlSessionFactory getSqlSessionFactory(@Autowired DataSource dataSource) {
         TransactionFactory transactionFactory = new JdbcTransactionFactory();
         Environment environment = new Environment("development", transactionFactory, dataSource);
         org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration(
@@ -26,5 +23,15 @@ public class SegmentConfig {
         configuration.addMapper(SegmentDao.class);
         configuration.setMapUnderscoreToCamelCase(true);
         return new SqlSessionFactoryBuilder().build(configuration);
+    }
+
+    @Bean
+    public DataSource getDataSource(@Autowired org.springframework.core.env.Environment environment) {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(environment.getProperty("spring.datasource.url"));
+        dataSource.setUsername(environment.getProperty("spring.datasource.username"));
+        dataSource.setPassword(environment.getProperty("spring.datasource.password"));
+        return dataSource;
     }
 }
